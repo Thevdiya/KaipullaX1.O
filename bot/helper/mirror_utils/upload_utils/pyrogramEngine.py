a1d5aef46ef804abc878a736c7050179fbb97706
Thevdiya <106153043+Thevdiya@users.noreply.github.com>
2022-06-24 16:01:04 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -68,7 +68,6 @@ class TgUploader:
 
     def __upload_file(self, up_path, file_, dirpath):
         if CUSTOM_FILENAME is not None:
-            cap_mono = cap_mono.replace(".mkv","")
             cap_mono = f"<b>{CUSTOM_FILENAME} {file_}\n\n 📥 JOIN : @KaipullaVadiveluoffl</b>"
             file_ = f"{CUSTOM_FILENAME} {file_}"
             new_path = ospath.join(dirpath, file_)
